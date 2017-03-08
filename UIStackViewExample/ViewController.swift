d6cdfdf13dd0a8719dea079724133e939e4cb833
sid2792 <sid.2792@gmail.com>
2017-03-08 11:13:56 +0530

    Initial Commit

---
+++ UIStackViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIStackViewExample
+//
+//  Created by RTS on 3/8/17.
+//  Copyright © 2017 RTS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
